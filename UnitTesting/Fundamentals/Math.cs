e10adf0a60647d04f80ca5994c74fc4402015118
MyoThet <mr.myothet@gmail.com>
2025-01-06 22:09:50 +0700

    black box tests

--- UnitTesting/Fundamentals/Math.cs
+++ UnitTesting/Fundamentals/Math.cs
@@ -1,3 +1,5 @@
+using System.Data;
+
 namespace UnitTesting.Fundamentals;
 
 public class Math
@@ -5,5 +7,10 @@ public class Math
     public int Add(int a, int b)
     {
         return a + b;
+    }
+
+    public int Max(int a, int b)
+    {
+        return (a > b) ? a : b;
     }
 }
